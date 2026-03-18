1120f30ae08e2d90db16f83862d6ebfb247fbb72
Babak Moghadas <ibsnetwork001@gmail.com>
2026-03-17 23:25:55 -0400

    Update OpenAI API key
    Replaced the OpenAI API key with a placeholder.

--- Pubmed/GPT.py
+++ Pubmed/GPT.py
@@ -4,7 +4,7 @@ import csv
 
 
 
-openai.api_key="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"
+openai.api_key="your API"
 
 # prompt = "The following is an abstract from a scientific paper. \n\nThe user want to extract the keywords related to autism. Can you generate a list of keywords\n\n"
 
